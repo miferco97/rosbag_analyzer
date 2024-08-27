72abf22a7b1b7cf878483685a0673d13d0b1629b
Miguel Fernandez Cortizas <miguel.fernandez.cortizas@upm.es>
2024-08-27 12:22:38 +0000

    refine view

--- utils/plot_functions.py
+++ utils/plot_functions.py
@@ -76,6 +76,9 @@ def plot3Dlines(lines,projection= None , x_lims= None,y_lims= None,z_lims= None 
         if len(projection) != 4:
             raise AssertionError("projection must be a 4 elem list (x,y,z,scale)")
         axes.get_proj = lambda: np.dot(Axes3D.get_proj(axes), np.diag(projection)) 
+
+    axes.get_proj = lambda: np.dot(Axes3D.get_proj(axes),np.diag([1,1,1,1])) 
+
     for i, line in enumerate(lines):
         if labels:
             label = labels[i]
@@ -88,16 +91,30 @@ def plot3Dlines(lines,projection= None , x_lims= None,y_lims= None,z_lims= None 
              [-0.5, 0.2, 1.91, 1.57]]
     
     plot_gate(GATES, axes)
+    x_lims = [-2,2.5]
+    y_lims = [-1.5,2]
+    z_lims = [0,2.5]
     
     if x_lims:
         axes.set_xlim3d(x_lims)
     if y_lims:
         axes.set_ylim3d(y_lims)
+    y_lims = [-1,2]
+    if z_lims:
+        axes.set_zlim3d(z_lims)
     # else:
     #     if np.max(np.abs(line2[1,:])) < 1:
     #         axes.set_ylim3d(-1,1)
     # if y_lims :
     #     axes.set_zlim3d(z_lims)
+
+    # y_tick = np.linspace(y_lims[0],y_lims[1],5)
+    y_tick = np.arange(y_lims[0],y_lims[1],1)
+    # add last element
+    y_tick = list(y_tick)
+    y_tick.append(y_lims[1])
+    axes.set_yticks(y_tick)
+    axes.view_init(elev=45, azim=-45)
     
     axes.legend()
     return axes
